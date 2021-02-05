4b3dd77074f7deb0daf6365cac493b2c45503272
Jammy12345 <jamwant.1995@gmail.com>
2021-02-05 16:44:05 +0530

    merging and authetication updated

---
+++ scripts/facewash.js
@@ -0,0 +1,142 @@
+window.addEventListener('load', function () {
+    loadData()
+  
+  })
+  let data = []
+  
+  async function loadData() {
+    try {
+      let response = await fetch('http://localhost:3000/facewash')
+      let data = await response.json();
+      let sortt = document.getElementById('sortBY')
+      sortt.addEventListener('change', function () {
+        let form = document.querySelector('form')
+        let formData = new FormData(form)
+        let sortBy = formData.get('sortBy')
+        console.log(sortBy);
+        if (sortBy == 'desc') {
+          function compare(a, b) {
+            var sumA = data.price
+            var sumB = data.price
+            if (sumA == sumB) {
+              return b.price - a.price
+            }
+            else {
+              return sumB - sumA;
+            }
+          }
+  
+          var detail = data.sort(compare)
+          displayData(data)
+        }
+        else {
+          function compare(a, b) {
+            var sumA = data.price
+            var sumB = data.price
+            if (sumA == sumB) {
+              return a.price - b.price
+            }
+            else {
+              return sumA - sumB;
+            }
+          }
+  
+          var detail = data.sort(compare)
+          console.log(detail);
+          displayData(data);
+        }
+      })
+      function compare(a, b) {
+        var sumA = data.price
+        var sumB = data.price
+        if (sumA == sumB) {
+          return b.price - a.price
+        }
+        else {
+          return sumB - sumA;
+        }
+      }
+  
+      var detail = data.sort(compare)
+      console.log(detail);
+      displayData(data);
+    }
+        
+  
+      catch (err) {
+          console.log(err)
+        }
+      }
+  
+  ;
+  
+      function displayData(data) {
+        let output = "";
+        for (i in data) {
+          output +=
+  
+            `<div class="product-card">
+                     <p id="tag" style="text-align: left; padding-left: 1rem;">${data[i].category}</p>
+                     <div class="product-pic">
+                         <img src=${data[i].imgUrl} alt="..."></img>
+                     </div>
+                     <div class="product-info">
+                         <p class="product-des">${data[i].des}</p>
+                         <p class="product-price">${data[i].price}</p>
+                     </div>
+                     <div id="btn" class="product-btn">
+                         <button class="heart" onclick="toggle()"><i class="fas fa-heart icon-cog"></i></button>
+                         <button class="add">ADD TO BAG</button>
+                     </div>
+                 </div>`
+  
+        }
+  
+        document.querySelector(".product").innerHTML = output;
+  
+      }
+  
+  
+  
+      const whiteHeart = '\u2661';
+      const blackHeart = '\u2665';
+      const button = document.querySelector('.heart');
+      button.addEventListener('click', toggle);
+  
+      function toggle() {
+        const like = button.textContent;
+        if (like == whiteHeart) {
+          button.textContent = blackHeart;
+        } else {
+          button.textContent = whiteHeart;
+        }
+      }
+  
+  
+  
+  
+      /* When the user clicks on the button,
+      toggle between hiding and showing the dropdown content */
+      function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+  
+      // Close the dropdown menu if the user clicks outside of it
+      window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
+  
+  
+  
+  
+  
+  
---
+++ scripts/wishlist.js
@@ -0,0 +1,85 @@
+window.addEventListener('load', showWishedData)
+function showWishedData() {
+    let data = JSON.parse(localStorage.getItem('product_wish'))
+    let output = ""
+    output += `<div class=wishHead><p>My Wishlist</p></div>`
+    for(i in data){
+        console.log(data[i].itemId)
+       // displayWish(data[i].itemImg,data[i].itemDesc,data[i].itemPrice,data[i].itemId)
+
+        
+        output += ` 
+                         
+                                   <div class= "wishCard">
+                                         <button class="wishDel" id="${data[i].id}" onclick="deleteWish(this)">X</button>
+                                         <img class="wishImg" src=${data[i].itemImg}></img>
+                                         <p>${data[i].itemDesc}</p>
+                                         <h3>${data[i].itemPrice}</h3>
+                                         <button class="moveToBag" id="${data[i].itemId}" onclick="moveToBag(this)">MOVE TO BAG</button>
+                                   </div>
+                     
+                     `
+      }
+      
+      document.getElementById('displayWish').innerHTML = output
+    
+}
+
+
+// function deleteWish(event) {
+//     id = event.id
+//     console.log(id)
+//     localStorage.remove('product_wish')
+// }
+
+// let display = document.getElementById('displayWish')
+
+// let temp = JSON.parse(localStorage.getItem('product_wish'))
+// localStorage.setItem('temp', JSON.stringify(temp))
+
+// function deleteWish(did){
+//     let added = localStorage.getItem("temp")
+//     let data = JSON.parse(added)
+//     let cart_remained = data.filter(el => {
+//         return (el.id !== did)
+//     })
+    
+//     localStorage.setItem('temp', JSON.stringify(cart_remained))
+   
+   
+    
+   
+   
+   
+// }
+
+//     let final = JSON.parse(localStorage.getItem('temp'))
+
+//     if(JSON.stringify(final) !== JSON.stringify([])){
+//         showWishedData(final)
+//     } else {
+//         remove()
+//     }
+    
+// // }
+
+//     function remove (){
+//         document.querySelector('#displayWish').style.display = 'block'
+//         display.style.display = 'none'
+//         localStorage.removeItem('cart-products')
+//         localStorage.removeItem('temp')
+      
+//     }
+
+
+
+
+function deleteWish(itemId, e) {
+    e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].itemId == itemId) {
+            // DO NOT CHANGE THE 1 HERE
+            data.splice(i, 1);
+        }
+    }
+}
